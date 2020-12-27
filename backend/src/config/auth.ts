c38bd19ab220c680a94a5f10ebfe3b61ca868a49
Hugo Rafael Alves <hugorafael.alves@hotmail.com>
2020-12-27 18:52:35 -0300

    first commit

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default{
+    jwt: {
+        secret: '89eda7a91936f665ea0b32fe7f94d4ac',
+        expiresIn: '1d'
+    }
+}
---
+++ backend/src/routes/sessions.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const sessionsRoutes = Router();
+
+sessionsRoutes.post('/', async (request, response)=>{
+
+    const { email, password } = request.body;
+
+    const autheticateUser = new AuthenticateUserService();
+
+    const { user, token } = await autheticateUser.execute({
+        email,
+        password
+    });
+
+    // FIXME ver uma forma melhora de deletar isso
+    delete user.password;
+
+    response.json({ user, token });
+
+});
+
+export default sessionsRoutes;
